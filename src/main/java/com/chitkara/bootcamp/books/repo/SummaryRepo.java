5f59ace7b4e63662688c35cef5d28f104ca99430
Shubhankar Sharma <sharmashubhankar19@gmail.com>
2025-02-16 22:37:16 +0530

    minor refactoring

--- src/main/java/com/chitkara/bootcamp/books/repo/SummaryRepo.java
+++ src/main/java/com/chitkara/bootcamp/books/repo/SummaryRepo.java
@@ -3,9 +3,9 @@ package com.chitkara.bootcamp.books.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.chitkara.bootcamp.books.entities.Summary;
+import com.chitkara.bootcamp.books.entities.SummaryEntity;
 
 @Repository
-public interface SummaryRepo extends JpaRepository<Summary, Long> {
+public interface SummaryRepo extends JpaRepository<SummaryEntity, Long> {
 
 }
--- src/main/java/com/chitkara/bootcamp/books/repo/UserRepo.java
+++ src/main/java/com/chitkara/bootcamp/books/repo/UserRepo.java
@@ -3,7 +3,7 @@ package com.chitkara.bootcamp.books.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.chitkara.bootcamp.books.entities.User;
+import com.chitkara.bootcamp.books.entities.UserEntity;
 
 
 public interface UserRepo 
